263e07047736a8c814b26c63d2bfdc19034f6309
nikhilmulik2002 <nikhilmulik2002@gmail.com>
2017-04-03 20:16:46 -0400

    First commit

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, jsonify, request
+from flask import render_template
+
+from decimal import *
+
+import simplejson
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def cover():
+    return render_template('index.html')
+
+
+
+
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
+
+
+
