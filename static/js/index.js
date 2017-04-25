7df00badf9a9f91395c1b3a91f45262d8ee9c3e0
Varad Shere <Varad Shere>
2017-04-25 16:42:40 -0400

    dash

--- static/js/index.js
+++ static/js/index.js
@@ -30,5 +30,28 @@ function login_post(val) {
     }
 }
 
+function fx(o) {
+  var $o = $(o);
+
+  $o.html($o.text().replace(/([\S])/g, "<span>$1</span>"));
+  $o.css("position", "relative");
+  $("span", $o).each(function(i) {
+    var newTop = Math.floor(Math.random()*500)*((i%2)?1:-1);
+    var newLeft = Math.floor(Math.random()*500)*((i%2)?1:-1);
+
+    $(this).css({position: "relative",
+      opacity: 1,
+      fontSize: 30,
+      top: 0,
+      left: 0
+    }).animate({
+      opacity: 0,
+      fontSize: 200,
+      top: newTop,
+      left:newLeft
+    },1000);
+  });
+}
+
 
 
