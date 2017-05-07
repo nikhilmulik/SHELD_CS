0ea1c53e0abcb603fa5bcce916a27a53ac43a6a3
puneetpalkaur <billingpuneet.13@gmail.com>
2017-05-06 21:07:33 -0400

    added method to set and read cookies

--- static/js/index.js
+++ static/js/index.js
@@ -1,6 +1,27 @@
 
 var  permission ='';
 
+function createCookie(name,value,days) {
+	if (days) {
+		var date = new Date();
+		date.setTime(date.getTime()+(days*24*60*60*1000));
+		var expires = "; expires="+date.toGMTString();
+	}
+	else var expires = "";
+	document.cookie = name+"="+value+expires+"; path=/ ; domain=;" ;
+}
+
+function readCookie(name) {
+	var nameEQ = name + "=";
+	var ca = document.cookie.split(';');
+	for(var i=0;i < ca.length;i++) {
+		var c = ca[i];
+		while (c.charAt(0)==' ') c = c.substring(1,c.length);
+		if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+	}
+	return null;
+}
+
 
 function login_post(val) {
     if(val == 'sign_in') {
@@ -20,9 +41,8 @@ function login_post(val) {
             traditional: true,
             dataType: "json",
             success: function (data) {
-                console.log(data);
-//                $.cookie("username", data.username);
-                localStorage.setItem('username', data.username);
+                //create cookie
+                createCookie('username',data.username,10);
 
                 window.location.href = "http://127.0.0.1:5000/dashboard";
             },
